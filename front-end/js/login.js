1bd0bafc406e9e3eb00a45b1f04c259b4345697e
Marcelo Vieira <vieiramarcelo.vds@gmail.com>
2025-11-27 18:08:18 -0300

    adicionando animação de ocultar e exibir senha ao clicar no icone de olho

---
+++ front-end/js/login.js
@@ -0,0 +1,29 @@
+/* *****************************************************************************************
+* Objetivo: animações e funcionalidades da pagina de login
+* Data: 27/11/2025
+* Versão: 1.0
+* *****************************************************************************************/
+
+'use strict'
+
+//ANIMAÇÕES
+
+//mostrar ou ocultar a senha no input ao clicar no icone de olho
+
+const inputSenha = document.getElementById("senha");
+const iconSenha = document.getElementById("icon-senha");
+
+iconSenha.addEventListener("click", () => {
+    const ehSenha = inputSenha.type == "password";
+
+    inputSenha.type = ehSenha ? "text" : "password";
+
+    if (ehSenha) {
+        iconSenha.classList.remove("bi-eye");
+        iconSenha.classList.add("bi-eye-slash");
+    } else {
+        iconSenha.classList.remove("bi-eye-slash");
+        iconSenha.classList.add("bi-eye");
+    }
+});
+
